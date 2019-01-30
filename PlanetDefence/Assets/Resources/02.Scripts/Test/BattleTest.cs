d8577897206ef025410be2a2f7277d107bd35adb
leeminsang <leeminsang19930211@gmail.com>
2019-01-30 09:20:24 +0900

    ver_1.21

--- PlanetDefence/Assets/Resources/02.Scripts/Test/BattleTest.cs
+++ PlanetDefence/Assets/Resources/02.Scripts/Test/BattleTest.cs
@@ -4,23 +4,10 @@ using UnityEngine;
 
 public class BattleTest : MonoBehaviour
 {
-    private int m_maxEnemCnt = 30;
-    private int m_actvEnemCnt = 0;
-
-
-    private int m_maxPlanetHP = 2000;
-    private int m_curPlanetHP = 2000;
-
-    private int m_gearCnt = 20;
-    private int m_eleCircuitCnt = 20;
-    private int m_coinCnt = 20;
-
     // Start is called before the first frame update
     void Start()
     {
-        BattleGameObjectMgr.Inst.UpdateEnemyCnt(m_maxEnemCnt, m_actvEnemCnt);
-        BattleGameObjectMgr.Inst.UpdatePlanetHP(m_maxPlanetHP, m_curPlanetHP);
-
+        
     }
 
     // Update is called once per frame
@@ -31,38 +18,9 @@ public class BattleTest : MonoBehaviour
             SceneLoader.LoadScene("Lobby");
         }
 
-        if(Input.GetKeyDown(KeyCode.F1))
-        {
-            m_actvEnemCnt += 1;
-
-            if (m_actvEnemCnt > m_maxEnemCnt)
-                m_actvEnemCnt = 0;
-
-            BattleGameObjectMgr.Inst.UpdateEnemyCnt(m_maxEnemCnt, m_actvEnemCnt);
-
-            m_curPlanetHP -= 20;
-
-            if (m_curPlanetHP < 0)
-                m_curPlanetHP = m_maxPlanetHP;
-
-            BattleGameObjectMgr.Inst.UpdatePlanetHP(m_maxPlanetHP, m_curPlanetHP);
-
-            m_gearCnt -= 1;
-            m_eleCircuitCnt -= 1;
-            m_coinCnt -= 1;
-
-            if (m_gearCnt < 0)
-                m_gearCnt = 20;
-            if (m_eleCircuitCnt < 0)
-                m_eleCircuitCnt = 20;
-            if (m_coinCnt < 0)
-                m_coinCnt = 20;
-
-            BattleGameObjectMgr.Inst.UpdateJunkCnt(m_gearCnt);
-            BattleGameObjectMgr.Inst.UpdateEleCircuitCnt(m_eleCircuitCnt);
-            BattleGameObjectMgr.Inst.UpdateCoinCnt(m_coinCnt);
-        }
-
-
+        //if (Input.touchCount > 0)
+        //{
+        //    SceneLoader.LoadScene("Lobby");
+        //}
     }
 }
