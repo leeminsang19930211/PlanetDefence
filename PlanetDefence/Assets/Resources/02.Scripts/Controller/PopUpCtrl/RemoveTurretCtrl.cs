b33d5dcc90a6b029d8854655b790227842e9fb4d
leeminsang <leeminsang19930211@gmail.com>
2019-02-05 10:30:27 +0900

    ver_1.71

---
+++ PlanetDefence/Assets/Resources/02.Scripts/Controller/PopUpCtrl/RemoveTurretCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveTurretCtrl : MonoBehaviour
+{
+    public void OnClick()
+    {
+        BattleGameObjectMgr.Inst.PopUpExit();
+        TurretMgr.Inst.RemoveTurretOnTurretSupport();
+    }
+}
