cbb3862889350cc68699c39e4f06ca302858e71e
leeminsang <leeminsang19930211@gmail.com>
2019-02-08 08:59:02 +0900

    ver_1.73

---
+++ PlanetDefence/Assets/Resources/02.Scripts/Data/Enum.cs
@@ -0,0 +1,30 @@
+﻿public enum Turret
+{
+    Lv1_Missile,
+    Lv1_Laser,
+    Lv2_Shield,
+}
+
+public enum Lab
+{
+    // Inc : Increasing
+    IncTurretSupports,
+    IncTurretHelth,
+    IncTurretDamage,
+}
+
+public enum SpaceShipPart
+{
+    _0,
+    _1,
+    _2,
+    _3,
+    _4
+}
+
+public enum BuyErr
+{
+   NoError,
+   AlreadySetUp,         // 터렛이 이미 설치되있는 경우
+   NotEnoughRsrc,        // 자원이 모자른 경우
+}
---
+++ PlanetDefence/Assets/Resources/02.Scripts/Manager/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    private static Player m_inst = null;
+    private int m_junk = 0;
+    private int m_eleCircuit = 0;
+    private int m_coin = 0;
+
+    public static Player Inst
+    {
+        get
+        {
+            if (m_inst == null)
+            {
+                GameObject container = new GameObject();
+                container.name = "Player";
+                m_inst = container.AddComponent<Player>() as Player;
+                DontDestroyOnLoad(container);
+            }
+
+            return m_inst;
+        }
+    }
+
+    /* 정보를 반환하는 함수들. 해금 여부, 비용등을 알수가 있다. */
+    public TurretInfo FindTurretInfo(Turret turret)
+    {
+        return new TurretInfo();
+    }
+
+    public LabInfo FindLabInfo(Lab lab)
+    {
+        return new LabInfo();
+    }
+
+    public SpaceShipPartInfo FindSpaceShipPartInfo(SpaceShipPart spcPart)
+    {
+        return new SpaceShipPartInfo();
+    }
+
+    // 터렛을 구매하고 생성한다. 포탑이 설치되어있는지 부터 체크한다.
+    public BuyErr BuyTurret(Turret turret)
+    {
+        return new BuyErr();
+    }
+
+    // 터렛을 판매하고 삭제한다. 삭제할 터렛이 없을경우 false 리턴한다.
+    public bool SellTurret()
+    {
+        return false;
+    }
+
+}
